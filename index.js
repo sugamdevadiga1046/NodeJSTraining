adbcb1325203157f456d9f344e852e20e2bbd2c3
KS SUGAM DEVADIGA <sugam.devadiga@niveussolutions.com>
2023-03-13 15:06:13 +0530

    New file creation with fs module and async/await

--- index.js
+++ index.js
@@ -11,3 +11,28 @@ displayMessage('Welcome to NodeJS!...').then(()=>{
 .catch((error)=>{
     console.error(error);
 })
+
+const fs = require('fs');
+const { resolve } = require('path');
+
+function createFile(fileName,data) {
+    return new Promise((resolve,reject)=>{
+        fs.writeFile(fileName, data, (error)=>{
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+(async function(){
+    try {
+        await createFile('File1.txt','Welcome to NodeJS fresher training');
+        console.log('Successfully file has been created');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
